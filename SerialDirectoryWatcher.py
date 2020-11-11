baa10069b65704f2289c9055f5793a30f4e917fc
Abhishek Patil <abhishek@zeroth.me>
2020-11-11 21:03:29 +1100

    dask map_blocks works as expected, now we have auto lut as well
    Signed-off-by: Abhishek Patil <abhishek@zeroth.me>

--- SerialDirectoryWatcher.py
+++ SerialDirectoryWatcher.py
@@ -1,17 +1,9 @@
 import os
 import sys
 import time
-from napari.qt.threading import WorkerBase
 from skimage.io.collection import alphanumeric_key
-from dask import delayed
-import dask.array as da
-from tifffile import imread
-from qtpy.QtCore import Signal, Slot
-from napari.qt.threading import WorkerBase, WorkerBaseSignals
-from typing import Type
 from napari.qt import thread_worker
-import logging
-logging.getLogger("tifffile").setLevel(logging.ERROR)
+
 
 def sort_files_by_channels(files, channels):
     file_channels = {}
@@ -24,82 +16,92 @@ def sort_files_by_channels(files, channels):
                 file_channels[channel].append(fileName)
     return file_channels
 
+
 @thread_worker
 def watch_dir(kwargs={}):
-    print(kwargs)
     path = kwargs.get("monitor_dir", "~")
     affine_mat = kwargs.get("affine", None)
-    channel_divider = kwargs.get("channel_divider", ["488nm", "560nm"])
-    flush_delay = int(kwargs.get("delay_between_frames", 10))
+    available_channels = kwargs.get("channel_divider", ["488nm", "560nm"])
+    delay_between_frames = int(kwargs.get("delay_between_frames", 10))
+    color_maps = ["bop purple", "bop orange", "bop blue", "green", "blue"]
+
+    channel_lut = {}
+    for index, channel in enumerate(available_channels):
+        channel_lut[channel] = color_maps[index % len(color_maps)]
+
     processed_files = {}
-    flush_timer = None
     last_file = {}
+    flush_timer = None
+
     while True:
         files_to_process = {}
 
         if not processed_files:
-            for channel in channel_divider:
+            for channel in available_channels:
                 processed_files[channel] = set()
-        
+
         if not files_to_process:
-            for channel in channel_divider:
-                
+            for channel in available_channels:
                 files_to_process[channel] = []
 
-        # Get the all files in the directory at this time
         current_files = set(os.listdir(path))
-        # print("current_files 1", current_files)
-        # channel separate
-        current_files = sort_files_by_channels(current_files, channel_divider)
-        # print("current_files 2", current_files)
-
+        current_files = sort_files_by_channels(current_files, available_channels)
 
         if flush_timer:
-            if (time.perf_counter() - flush_timer) > flush_delay:
-                for channel in channel_divider:
+            # check if the timer has passed the delay_between_frames
+            if (time.perf_counter() - flush_timer) > delay_between_frames:
+                for channel in available_channels:
                     if last_file.get(channel, None):
                         if channel in last_file[channel]:
                             # reset the flush timer
+                            processed_files[channel].update(set([last_file[channel]]))
                             flush_timer = None
+                            files = [
+                                os.path.join(path, fileName)
+                                for fileName in processed_files[channel]
+                            ]
+                            files = sorted(files, key=alphanumeric_key)
+
                             yield {
-                                "image": os.path.join(path, last_file[channel]), #delayed(imread)(os.path.join(path, last_file[channel])),
+                                "image": files,
                                 "channel": channel,
-                                "affine": affine_mat
+                                "affine": affine_mat,
+                                "lut": channel_lut[channel],
                             }
-                            processed_files[channel].update(set(last_file[channel]))
-                            last_file = {}
-        # print("current_files ",current_files)
+                            last_file[channel] = None
+
         if len(current_files):
-            # print("here")
-            for channel in channel_divider:
+            for channel in available_channels:
                 if len(current_files[channel]):
-                    # print("Channel ", channel)
-                    # print("files ", current_files[channel])
-                    last_file[channel] = sorted(current_files[channel], key=alphanumeric_key)[-1]
+                    last_file[channel] = sorted(
+                        current_files[channel], key=alphanumeric_key
+                    )[-1]
                     current_files[channel].remove(last_file[channel])
-                    # print("current_files[channel] ", current_files[channel])
-                    files_to_process[channel] = list(set(current_files[channel]) - set(processed_files[channel]))
-                    # print("files_to_process[channel] ", files_to_process[channel])
+                    files_to_process[channel] = list(
+                        set(current_files[channel]) - set(processed_files[channel])
+                    )
 
-        # yield every file to process as a dask.delayed function object.
-        # print("Ready to emit ", files_to_process)
-        for channel in channel_divider:
-            for p in sorted(files_to_process[channel], key=alphanumeric_key):
-                    # reset the flush timer
-                    # print(channel, p)
-                    flush_timer = time.perf_counter()
-
-                    yield  {
-                        "image":os.path.join(path, p), #delayed(imread)(os.path.join(path, p)),
-                        "channel": channel,
-                        "affine": affine_mat
-                    }
-                    processed_files[channel].add(p)
+        # yield all the files so far process to use it with dask.map_blocks.
+        for channel in available_channels:
+            files_to_display = files_to_process[channel]
+            if len(files_to_display):
+                # update flush timer
+                flush_timer = time.perf_counter()
+                processed_files[channel].update(files_to_display)
+                files = [
+                    os.path.join(path, fileName)
+                    for fileName in processed_files[channel]
+                ]
+                files = sorted(files, key=alphanumeric_key)
+                yield {
+                    "image": files,
+                    "channel": channel,
+                    "affine": affine_mat,
+                    "lut": channel_lut[channel],
+                }
             else:
                 yield {}
         else:
             yield {}
 
-        # add the files which we have yield to the processed list.
-        # processed_files.update(files_to_process)
         time.sleep(0.1)
--- main.py
+++ main.py
@@ -2,68 +2,87 @@ import os
 import sys
 import time
 import numpy as np
+import napari
 from skimage.io.collection import alphanumeric_key
-from dask import delayed
 import dask.array as da
 from tifffile import imread
-import napari
 from magicgui import magicgui
 from pathlib import Path
-# from SerialDirectoryWatcher import SerialDirectoryWatcher
-import logging
-logging.getLogger("tifffile").setLevel(logging.ERROR)
-from dask.array.core import normalize_chunks
 from SerialDirectoryWatcher import watch_dir
 
+# disable tifffile warnings
+import logging
+
+logging.getLogger("tifffile").setLevel(logging.ERROR)
+
+
+def read_one_timepoint(fname_array, block_id):
+    # block_id will be something like (t, 0, 0, 0)
+    file_num = block_id[0]
+    filename = fname_array[file_num]
+    single_timepoint = imread(filename)
+    reshaped_to_chunk_shape = single_timepoint[np.newaxis, :, :, :]
+    # return the chunk that map_blocks expects
+    return reshaped_to_chunk_shape
+
+
+def delayed_multi_imread(fname_array, image_shape, image_dtype):
+    nt = fname_array.shape[0]
+    nz, ny, nx = image_shape
+    image = da.map_blocks(
+        read_one_timepoint,
+        fname_array,
+        # shape of one chunk = (1, nz, ny, nx)
+        # all chunks together: ((1, 1, 1, 1, ...), )
+        chunks=((1,) * nt, (nz,), (ny,), (nx,)),
+        dtype=image_dtype,
+    )
+    return image
+
+
 worker = None
-total_frames = 0
+
+
 with napari.gui_qt():
     viewer = napari.Viewer(ndisplay=3)
-    
+
     channel_layers = {}
 
-    def dask_map_func(fname, current_data, block_info=None):
-        image = imread(fname)
-        image = image.reshape((1,) + image.shape)
-        r = da.concatenate((current_data, image), axis=0)
-        return r
-
     def append(data):
-        global total_frames
-        
-        delayed_image = data.get("image", None)
+
+        all_filenames = np.asarray(data.get("image", []))
+        if len(all_filenames) == 0:
+            return
         affine_mat = data.get("affine", None)
         channel = data.get("channel", None)
-
-        if delayed_image is None:
-            return
-        
-        total_frames = total_frames + 1
+        lut = data.get("lut", "bop purple")
 
         if (channel in channel_layers) and viewer.layers:
             # layer is present, append to its data
             layer = channel_layers[channel]
 
-            image_shape=layer.data.shape
+            if len(all_filenames) == layer.data.shape[0]:
+                return
+
+            image_shape = layer.data.shape[1:]
             image_dtype = layer.data.dtype
-            chunk_size = (total_frames, ) + image_shape[1:]
-            
-            layer.data = da.map_blocks(
-                dask_map_func,
-                chunks=chunk_size,
-                fname = delayed_image,
-                current_data = layer.data,
-                dtype=image_dtype,
-                # meta=np.asanyarray([])
-            )
-           
+
+            layer.data = delayed_multi_imread(all_filenames, image_shape, image_dtype)
             layer.affine = affine_mat
         else:
             # first run, no layer added yet
-            image = imread(delayed_image)
-            image = image.reshape((1,) + image.shape)
-            channel_layers[channel] = viewer.add_image(image, affine=affine_mat, name=channel, rendering='attenuated_mip')
-            
+            image0 = imread(all_filenames[0])
+            image_dtype = image0.dtype
+            image_shape = image0.shape
+            image = delayed_multi_imread(all_filenames, image_shape, image_dtype)
+            channel_layers[channel] = viewer.add_image(
+                image,
+                affine=affine_mat,
+                name=channel,
+                rendering="mip",
+                blending="additive",
+                colormap=lut,
+            )
 
         if len(viewer.layers):
             viewer.dims.set_point(0, 0)
@@ -71,39 +90,56 @@ with napari.gui_qt():
         # if viewer.dims.point[0] >= layer.data.shape[0] - 2:
         #     viewer.dims.set_point(0, layer.data.shape[0] - 1)
 
+    @magicgui(
+        dx={"decimals": 4},
+        dy={"decimals": 4},
+        dz={"decimals": 4},
+        angle={"decimals": 4},
+        monitor_dir={"mode": "D"},
+        layout="form",
+        call_button="Start/Update"
+    )
+    def deskew_settings(
+        angle: float = 31.8,
+        dx: float = 0.104,
+        dy: float = 0.104,
+        dz: float = 0.4,
+        delay_between_frames: int = 10,
+        channel_divider: str = "488nm, 560nm",
+        monitor_dir=Path("~")
+    ):
 
-    @magicgui ( dx= {"decimals":4 }, dy= {"decimals":4 }, dz= {"decimals":4 },
-                angle= {"decimals":4}, monitor_dir={"mode":"D"}, layout="form",
-                call_button="Start/Update" )
-    def deskew_settings(angle: float = 31.8, dx: float = 0.104, 
-                        dy: float = 0.104, dz: float= 0.4, 
-                        delay_between_frames: int = 10, 
-                        channel_divider: str = "488nm, 560nm", monitor_dir=Path("~"),
-                        # running: bool = False
-                        ):
-        
-        dz_tan = np.tan(np.deg2rad(90-angle))
-        dz_sin = np.sin(np.deg2rad(angle))* dz
-        shear = np.array([
-                [ 1.,      0.,  0.,  0., 0.],
-                [ 0.,      1.,  0.,  0., 0.],
-                [ 0.,      0.,  1.,  0., 0.],
-                [ 0.,  dz_tan,  0.,  1., 0.],
-                [ 0,       0.,  0.,  0., 1.]])
+        dz_tan = np.tan(np.deg2rad(90 - angle))
+        dz_sin = np.sin(np.deg2rad(angle)) * dz
+        shear = np.array(
+            [
+                [1.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0, 0.0],
+                [0.0, dz_tan, 0.0, 1.0, 0.0],
+                [0, 0.0, 0.0, 0.0, 1.0],
+            ]
+        )
 
-        scale = np.array([
-                [ 1., 0.,     0.,   0.,  0.],
-                [ 0., dz_sin, 0.,   0.,  0.],
-                [ 0., 0.,     dy,   0.,  0.],
-                [ 0., 0.,     0.,   dx,  0.],
-                [ 0., 0.,     0.,   0.,  1.]])
+        scale = np.array(
+            [
+                [1.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, dz_sin, 0.0, 0.0, 0.0],
+                [0.0, 0.0, dy, 0.0, 0.0],
+                [0.0, 0.0, 0.0, dx, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 1.0],
+            ]
+        )
 
-        global_affine =shear @ scale
-        channel_divider_list = [ i.strip() for i in channel_divider.split(",")]
-        return {"monitor_dir": monitor_dir, "affine": global_affine, 
-                "delay_between_frames": delay_between_frames, 
-                "channel_divider": channel_divider_list }
-    
+        global_affine = shear @ scale
+        channel_divider_list = [i.strip() for i in channel_divider.split(",")]
+        return {
+            "monitor_dir": monitor_dir,
+            "affine": global_affine,
+            "delay_between_frames": delay_between_frames,
+            "channel_divider": channel_divider_list,
+        }
+
     deskew_settings_widget = deskew_settings.Gui()
 
     def start_monitoring(args):
@@ -115,21 +151,17 @@ with napari.gui_qt():
             worker.start()
         else:
             print("re-started")
-            # worker.stop()
+            worker.quit()
             del worker
-            
+
             viewer.layers.select_all()
             viewer.layers.remove_selected()
             channel_layers.clear()
             worker = watch_dir(args)
             worker.yielded.connect(append)
             worker.start()
-        
-    
-    deskew_settings_widget.called.connect(start_monitoring)
-    viewer.window.add_dock_widget(deskew_settings_widget, 
-                                name="Start Live Deskew", area='right')
-    
 
-    
-        
+    deskew_settings_widget.called.connect(start_monitoring)
+    viewer.window.add_dock_widget(
+        deskew_settings_widget, name="Start Live Deskew", area="right"
+    )
